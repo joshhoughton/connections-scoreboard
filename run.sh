9636d56d5968273f248c854e7887da0068dad9f4
Josh Houghton <j@joshhoughton.io>
2024-02-06 23:42:02 +0000

    Add /app dir to PYTHONPATH

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export FLASK_APP=/app/connections_scoreboard/app
+export PYTHONPATH=$PYTHONPATH:/app/
 
 python3 -u /app/connections_scoreboard/bot/main.py &
 python3 -m flask run --host=0.0.0.0
