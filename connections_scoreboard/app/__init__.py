b71648872684d24cea61d2c4a7eafca4f04842d7
Josh Houghton <j@joshhoughton.io>
2024-02-08 16:59:58 +0000

    Add middleware for NPM

--- connections_scoreboard/app/__init__.py
+++ connections_scoreboard/app/__init__.py
@@ -1,18 +1,29 @@
+import logging
+
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 
 from connections_scoreboard.app.config import Config
 from connections_scoreboard.app.db import db
+from connections_scoreboard.app.middleware import RealIPMiddleware
+
+
+# from connections_scoreboard.app.db.models import Result, User
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.wsgi_app = RealIPMiddleware(app.wsgi_app)
+
+    bootstrap = Bootstrap5(app)
+    app.logger.setLevel(logging.DEBUG)
 
     app.config.from_object(Config)
 
     db.init_app(app)
 
     with app.app_context():
-        # db.drop_all()
         db.create_all()
 
     from .routes import bp as main_bp
---
+++ connections_scoreboard/app/middleware.py
@@ -0,0 +1,8 @@
+class RealIPMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        if "HTTP_X_REAL_IP" in environ:
+            environ["REMOTE_ADDR"] = environ["HTTP_X_REAL_IP"]
+        return self.app(environ, start_response)
